16dcd1ba8205e995787abc0e2110be96f081c58f
zer0who <eagle7449@naver.com>
2022-08-11 18:35:58 +0900

    commit: django login_2

--- login/admin.py
+++ login/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import CloudUser
 
 # Register your models here.
-admin.site.register(CloudUser)
+#admin.site.register(CloudUser)
+
+@admin.register(CloudUser)
+class CloudUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'birth_date', 'email', 'created_at', 'updated_at']
--- login/models.py
+++ login/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class CloudUser(models.Model):
     name = models.TextField()
+    birth_date = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #모델을 통해 생성할 때 생성 시각이 자동으로 입력되게
     updated_at = models.DateTimeField(auto_now=True)    #모델을 통해 업데이트할 때 수정 시각이 자동으로 입력되게 
 
