95a251b0ded7e5b3f225a59590359bf03b4aa7e4
zer0who <eagle7449@naver.com>
2022-08-11 17:36:21 +0900

    commit: django login_1st

---
+++ login/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class CloudUser(models.Model):
+    name = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) #모델을 통해 생성할 때 생성 시각이 자동으로 입력되게
+    updated_at = models.DateTimeField(auto_now=True)    #모델을 통해 업데이트할 때 수정 시각이 자동으로 입력되게 
+
+    def __str__(self):  #java의 toString과 같은 느낌
+        return self.name
