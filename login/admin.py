2859b0824705407b62ff874bea901c8cae9e3d22
zer0who <eagle7449@naver.com>
2022-08-13 19:24:04 +0900

    commit: django add annotation to login app

--- login/admin.py
+++ login/admin.py
@@ -4,6 +4,6 @@ from .models import CloudUser
 # Register your models here.
 #admin.site.register(CloudUser)
 
-@admin.register(CloudUser)
+@admin.register(CloudUser)  #admin페이지 연습한다고 등록해놓은 model
 class CloudUserAdmin(admin.ModelAdmin):
     list_display = ['user_id', 'user_pw', 'user_name', 'user_email', 'user_birthdate']
--- login/views.py
+++ login/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import CloudUser
 # Create your views here.
 
-def CloudUser_list(request):
-    qs = CloudUser.objects.all()
-    return render(request, 'login/CloudUser_list.html', {
-        'CloudUser_list': qs,
-    })
+# def CloudUser_list(request):  #예전에 연습한다고 만들어놓은 model의 view임. 신경 끌 것.
+#     qs = CloudUser.objects.all()
+#     return render(request, 'login/CloudUser_list.html', {
+#         'CloudUser_list': qs,
+#     })
